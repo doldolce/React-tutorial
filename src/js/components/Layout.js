1725a51b1f6cd791e69c7f23e103ffb277effa4f
naoya <naoya@developMBP.local>
2019-05-19 00:47:40 +0900

    about state

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -3,10 +3,18 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component{
+    constructor(){
+        super();
+        this.state = {name: "doldol"};
+    }
     render(){
+        setTimeout(
+            () => {this.setState({name: "Hello"});}
+        , 1000);
         let components = [<Header />, <Footer />];
         return(
             <div>
+                {this.state.name}
                 {components}
             </div>
         );
